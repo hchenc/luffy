c9ef0eb24dce598437f2bfdab32c006aa291de33
hchenc <happytime_ch@foxmail.com>
2022-12-09 17:48:11 +0800

    fix bug on shell command flag

--- cmd/server.go
+++ cmd/server.go
@@ -15,13 +15,7 @@ import (
 // ServerCmd represents the server command
 var ServerCmd = &cobra.Command{
 	Use:   "server",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "command server",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(os.Args)
 		go etcdmain.Main(checkArgs(os.Args))
--- cmd/shell.go
+++ cmd/shell.go
@@ -11,18 +11,13 @@ import (
 )
 
 var cfgFile string
+var command string
 var server *host.Config
 
 // ShellCmd represents the shell command
 var ShellCmd = &cobra.Command{
 	Use:   "shell",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "run shell command on remote hosts",
 	Run: func(cmd *cobra.Command, args []string) {
 		content, err := os.ReadFile(cfgFile)
 		if err != nil {
@@ -43,6 +38,9 @@ to quickly create a Cobra application.`,
 			if err != nil {
 				errs = append(errs, err)
 			}
+			if command != "" {
+				server.Do = command
+			}
 			output, code, err := c.Exec(host, server.Do)
 			if err != nil {
 				errs = append(errs, err)
@@ -54,5 +52,6 @@ to quickly create a Cobra application.`,
 }
 
 func init() {
-	ServerCmd.Flags().StringVar(&cfgFile, "config", "/Users/joey/.luffy.yaml", "config file (default is $HOME/.luffy.yaml)")
+	ShellCmd.Flags().StringVar(&cfgFile, "config", "/Users/joey/.luffy.yaml", "config file (default is $HOME/.luffy.yaml)")
+	ShellCmd.Flags().StringVar(&command, "cmd", "", "shell command")
 }
